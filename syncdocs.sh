1337e452f3c7a6aa474ff60059a2431f52e6552d
Adam Vogt <vogt.adam@gmail.com>
2014-01-27 01:32:16 -0500

    more packages in docs

--- syncdocs.sh
+++ syncdocs.sh
@@ -3,8 +3,14 @@ set -e
 if [[ ! (-d "HList-0.3.0.1") ]]; then
     cabal unpack HList
 fi
+if [[ ! (-d "numtype-tf-0.1.1") ]]; then
+    cabal unpack numtype-tf
+fi
+if [[ ! (-d "dimensional-tf-0.2.1") ]]; then
+    cabal unpack dimensional-tf
+fi
 # https://github.com/feuerbach/standalone-haddock
-standalone-haddock --package-db=$HOME/.ghc/x86_64-linux-7.6.2/package.conf.d -o doc . HList-*
+standalone-haddock --hyperlink-source --package-db=$HOME/.ghc/x86_64-linux-7.6.2/package.conf.d -o doc . HList-* numtype-tf-* dimensional-tf-*
 rsync -av --delete-after doc/ ../aavogt.github.io/haddock/DimMat
 cd ../aavogt.github.io
 git add -A haddock
