2eaa5d0168c959a9f29acee7ed685d902c65dae5
Adam Vogt <vogt.adam@gmail.com>
2014-01-27 19:23:59 -0500

    use the right quotes in syncdocs

--- syncdocs.sh
+++ syncdocs.sh
@@ -14,5 +14,5 @@ standalone-haddock --hyperlink-source --package-db=$HOME/.ghc/x86_64-linux-7.6.2
 rsync -av --delete-after doc/ ../aavogt.github.io/haddock/DimMat
 cd ../aavogt.github.io
 git add -A haddock
-git commit -m 'DimMat $(date)'
+git commit -m "DimMat $(date)"
 git push --mirror
